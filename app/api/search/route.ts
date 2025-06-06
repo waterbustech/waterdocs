002b16cff4947c121d7b14047678f3154df3c3a1
Kai <lambiengcode@gmail.com>
2025-06-06 21:54:16 +0700

    docs: update .

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,4 +1,19 @@
 import { source } from '@/lib/source';
 import { createFromSource } from 'fumadocs-core/search/server';
 
-export const { GET } = createFromSource(source);
+export const { GET } = createFromSource(source, {
+  localeMap: {
+    // you can customise search configs for specific locales, like:
+    // [locale]: Orama options
+
+    vi: {
+      components: {
+        // tokenizer: createTokenizer(),
+      },
+      search: {
+        threshold: 0,
+        tolerance: 0,
+      },
+    },
+  },
+});

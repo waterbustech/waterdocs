6501955bc685dc625afcee72b9fb927e1d98e083
Kai <lambiengcode@gmail.com>
2025-07-20 09:45:58 +0700

    chore: update openapi

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -8,4 +8,24 @@ void generateFiles({
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
   includeDescription: true,
+  per: 'operation',
+  groupBy: 'tag',
+  name: (output, document) => {
+    if (output.type === "operation") {
+      const operation = document.paths[output.item.path][output.item.method];
+      const operationId =
+        operation.operationId || operation.summary || "unnamed";
+      const lastPart = operationId.split(".").pop();
+      return `${lastPart}`;
+    }
+
+    if (output.type === "webhook") {
+      const hook = document.webhooks[output.item.name][output.item.method];
+      const hookId = hook.operationId || hook.summary || "unnamed";
+      const lastPart = hookId.split(".").pop();
+      return `${lastPart}`;
+    }
+
+    return "unknown";
+  },
 });
